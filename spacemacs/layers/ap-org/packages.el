c4afc1f97dc252354a142555bc6140cd9c521d51
Alexander Pehm <alexander.pehm@paysafe.com>
2019-12-08 22:08:20 +0100

    org-brain config

--- spacemacs/layers/ap-org/packages.el
+++ spacemacs/layers/ap-org/packages.el
@@ -122,6 +122,23 @@
 
 (require 'org-drill)
 
+(use-package org-brain :ensure t
+  :init
+  (setq org-brain-path spacemacs-workspace)
+  ;; For Evil users
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12)
+  (setq org-brain-include-file-entries nil
+        org-brain-file-entries-use-title nil))
+
 (setq org-todo-keyword-faces
           '(("TODO" . (:foreground "white" :weight bold)) ("STARTED" . "yellow")
             ("DONE" . (:foreground "green" :weight bold))))
